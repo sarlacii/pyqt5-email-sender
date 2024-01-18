09b2f63de2b12e392ed29baa58fbe2a03edf727b
Julian <julian@finmon.co.za>
2024-01-18 15:14:23 +0200

    Added updated SMTP list for ZA

--- email_sender.py
+++ email_sender.py
@@ -1,4 +1,5 @@
 import smtplib, ssl, time
+#from requests import get
 
 level = 2
 
@@ -15,19 +16,22 @@ def send_email_ssl(recipient, email):
         print('Sent')
         smtp.close()
 
-def send_email(recipient, email, sender):
-    port = 465
+def send_email(recipient, email, sender, smtp):
+    port = 25
+
+#    ip = get('https://api.ipify.org').content.decode('utf8')
+#    print('My public IP address is: {}'.format(ip))
 
     #sender='thesender@dnka.co.za'
     #receiver='julian@finmon.co.za'
     #password=''
 
     #smtpserver=smtplib.SMTP("smtp.saix.net",587)
-    smtpserver=smtplib.SMTP("smtp.saix.net",25)
+    smtpserver=smtplib.SMTP(smtp,port)
     smtpserver.set_debuglevel(level)
-    #smtpserver.ehlo()
+    smtpserver.ehlo('smtp.' + sender.split('@')[1])
     #smtpserver.starttls()
-    smtpserver.ehlo
+    #smtpserver.ehlo
     time.sleep(1)
     #smtpserver.login(sender,password)
     #msg='Subject:Demo\nThis is a demo'
@@ -35,3 +39,8 @@ def send_email(recipient, email, sender):
     smtpserver.sendmail(sender, recipient, email)
     print('Sent')
     smtpserver.close()
+    return(1)
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(send_email("<your-user>@<your-email.domain>","Subject: Hi there!\n\nHowzit!\n","fjhgqtaa@sharklasers.com"))
--- main.py
+++ main.py
@@ -7,20 +7,28 @@ class EmailSender(QMainWindow):
     def __init__(self):
         super(EmailSender, self).__init__()
         loadUi("main.ui", self)
-        
+
         self.emailButton.clicked.connect(self.send_email)
-        
+
     def send_email(self):
-        msg='To:' + self.lineEdit.text() + '\nSubject:Quick request please!\n'
-        if self.lineEdit.text():
-            send_email(recipient=self.lineEdit.text(), email=str("%s%s" % (msg, self.textEdit.toPlainText())), sender=self.lineEdit_sender.text())
-        else:
-            message = QMessageBox()
-            message.setIcon(QMessageBox.Critical)
-            message.setText("Invalid recipient")
-            message.setWindowTitle("Error!")
-            message.exec_()
-        
+        msg='To:' + self.lineEdit.text() + '\nSubject:' + self.lineEdit_subject.text() + '\n'
+        result = 0
+        try:
+            if self.lineEdit.text():
+                result = send_email(recipient=self.lineEdit.text(), email=str("%s%s" % (msg, self.textEdit.toPlainText())), sender=self.lineEdit_sender.text(), smtp=str(self.comboBox_smtp.currentText()))
+            else:
+                message = QMessageBox()
+                message.setIcon(QMessageBox.Critical)
+                message.setText("Invalid recipient")
+                message.setWindowTitle("Error!")
+                message.exec_()
+        except Exception as e:
+            print(e)
+
+        if result >= 1:
+            self.lineEdit_result.setText("Sent email to " + self.lineEdit.text())
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = EmailSender()
